ec9e3bf689b8d2cf1fae9fc869548432ee0e6309
Sandhya2803 <sandhya.pidishetti@slu.edu>
2024-11-04 00:11:10 +0000

    final changes

--- app.js
+++ app.js
@@ -27,6 +27,123 @@ document.addEventListener('DOMContentLoaded', () => {
     // find the user object in our data
     const id = document.querySelector('#userID').value;
 
+    for (let i=0; i<users.length; i++) {
+        // found relevant user, so update object at this index and redisplay
+        if (users[i].id == id) {
+
+            users[i].user.firstname = document.querySelector('#firstname').value;
+            users[i].user.lastname = document.querySelector('#lastname').value;
+            users[i].user.address = document.querySelector('#address').value;
+            users[i].user.city = document.querySelector('#city').value;
+            users[i].user.email = document.querySelector('#email').value;     
+
+            generateUserList(users, stocks);
+        }
+    }
   });
+  });
+  
+  /**
+ * Loops through the users and renders a ul with li elements for each user
+ * @param {*} users 
+ */
+function generateUserList(users) {
+    // get the list element and for each user create a list item and append it to the list
+    const userList = document.querySelector('.user-list');
+    users.map(({ user, id }) => {
+      const listItem = document.createElement('li');
+      listItem.innerText = user.lastname + ', ' + user.firstname;
+      listItem.setAttribute('id', id);
+      userList.appendChild(listItem);
+    });
+  
+    // register the event listener on the list
+    userList.addEventListener('click', (event) => handleUserListClick(event, users));
+  }
+
+  /**
+ * Handles the click event on the user list
+ * @param {*} event 
+ */
+function handleUserListClick(event, users) {
+    // get the user id from the list item
+    const userId = event.target.id;
+    // find the user in the userData array
+    // we use a "truthy" comparison here because the user id is a number and the event target id is a string
+    const user = users.find(user => user.id == userId);
+    // populate the form with the user's data
+    populateForm(user);
+  }
+
+  /**
+ * Populates the form with the user's data
+ * @param {*} user 
+ */
+function populateForm(data) {
+    // use destructuring to get the user and id from the data object
+    const { user, id } = data;
+    document.querySelector('#userID').value = id;
+    document.querySelector('#firstname').value = user.firstname;
+    document.querySelector('#lastname').value = user.lastname;
+    document.querySelector('#address').value = user.address;
+    document.querySelector('#city').value = user.city;
+    document.querySelector('#email').value = user.email;
+  }
+
+  /**
+ * Renders the portfolio items for the user
+ * @param {*} user 
+ */
+function renderPortfolio(user, stocks) {
+    // get the user's stock data
+    const { portfolio } = user;
+    // get the portfolio list element
+    const portfolioDetails = document.querySelector('.portfolio-list');
+    // clear the list from previous render
+    portfolioDetails.innerHTML = '';
+    // map over portfolio items and render them
+    portfolio.map(({ symbol, owned }) => {
+      // create a list item and append it to the list
+      const symbolEl = document.createElement('p');
+      const sharesEl = document.createElement('p');
+      const actionEl = document.createElement('button');
+      symbolEl.innerText = symbol;
+      sharesEl.innerText = owned;
+      actionEl.innerText = 'View';
+      actionEl.setAttribute('id', symbol);
+      portfolioDetails.appendChild(symbolEl);
+      portfolioDetails.appendChild(sharesEl);
+      portfolioDetails.appendChild(actionEl);
+    });
+  }
+
+  // register the event listener on the list
+  portfolioDetails.addEventListener('click', (event) => {
+    // let's make sure we only handle clicks on the buttons
+    if (event.target.tagName === 'BUTTON') {
+      viewStock(event.target.id, stocks);
+    }
+  });
+
+  /**
+ * Renders the stock information for the symbol
+ * @param {*} symbol 
+ */
+function viewStock(symbol, stocks) {
+    // begin by hiding the stock area until a stock is viewed
+    const stockArea = document.querySelector('.stock-form');
+    if (stockArea) {
+        // find the stock object for this symbol
+        const stock = stocks.find( function (s) { return s.symbol == symbol;});
+  
+        document.querySelector('#stockName').textContent = stock.name;
+        document.querySelector('#stockSector').textContent = stock.sector;
+        document.querySelector('#stockIndustry').textContent = stock.subIndustry;
+        document.querySelector('#stockAddress').textContent = stock.address;
+  
+        document.querySelector('#logo').src = `logos/${symbol}.svg`;
+    }
+  }
+
 
 
